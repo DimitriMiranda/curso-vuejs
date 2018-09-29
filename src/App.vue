d1d5008e522bd2cfcf9456bf514ab9b57e3939dc
DimitriMiranda <dimitri.miranda@gmail.com>
2018-09-29 13:45:58 -0500

    finalizando o capitulo 3

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Tarefas</h1>
+      <input-task></input-task>
+    </header>
+  </section>
+</template>
+
+<script>
+import InputTask from './components/InputTask'
+
+export default {
+  name: 'App',
+  components: {
+    InputTask
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/InputTask.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input class="new-todo"
+     @keyup.enter="addTask"
+    placeholder="O que precisa ser feito?">
+  </div>
+</template>
+
+<script>
+import { Task } from '../models/Task'
+
+export default {
+  name: 'IputTask',
+  data () {
+    return {}
+  },
+  methods: {
+    addTask ($event) {
+      let task = new Task()
+      task.completed = false
+      task.title = $event.target.value
+      console.log(task)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
